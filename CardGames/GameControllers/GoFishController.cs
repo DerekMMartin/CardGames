75227dbbcfa44e08da78853876a805f02c1409a3
William Walsh <wwalsh@student.neumont.edu>
2017-10-25 09:40:47 -0600

    Added constructor to GoFishController

--- CardGames/GameControllers/GoFishController.cs
+++ CardGames/GameControllers/GoFishController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CardGames.Models;
+using CardGames.Models.Player;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,21 @@ namespace CardGames.GameControllers
 {
     public class GoFishController : Game
     {
+        public GoFishController(int players, List<string> playerNames)
+        {
+            for (int i = 0; i < players; i++)
+            {
+                GoFishPlayer p = new GoFishPlayer();
+
+                p.Name = playerNames[i];
+                p.IsAi = false;
+
+                p.Hand = new List<Card>();
+
+                Players.Add(p);
+            }
+        }
+
+
     }
 }
