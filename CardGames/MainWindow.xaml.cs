0fc16fb27fdec9204fe607f2354410fb3a09343f
Matthew Woolhiser <MWoolhiser@student.neumont.edu>
2017-10-26 09:20:15 -0600

    migrated main menu to a UC

--- CardGames/MainWindow.xaml.cs
+++ CardGames/MainWindow.xaml.cs
@@ -24,22 +24,9 @@ namespace CardGames
         public MainWindow()
         {
             InitializeComponent();
+            MenuUC menu = new MenuUC(this);
+            Root.Children.Add(menu);
         }
 
-        private void BlackJackButton_Click(object sender, RoutedEventArgs e)
-        {
-            Root.Children.Clear();
-            Root.Children.Add(new BlackJackStartMenu());
-        }
-
-        private void GoFishButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void WarButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
