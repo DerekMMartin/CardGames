fa9fe812a289a14f37daa93337343d3e8fdb0795
KingofPawns <dvandeveer@student.neumont.edu>
2017-10-26 09:36:26 -0600

    Did not actually add WarSetupControl.xaml with last commit

---
+++ CardGames/UserControls/WarSetupControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CardGames.UserControls
+{
+    /// <summary>
+    /// Interaction logic for WarSetupControl.xaml
+    /// </summary>
+    public partial class WarSetupControl : UserControl
+    {
+        public WarSetupControl()
+        {
+            InitializeComponent();
+        }
+
+        private void PVPButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PVCButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
