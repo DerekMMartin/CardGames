5604b6a8e788a9544a2af94e93d08751a61d5f67
DerekM197 <Derekmikelmartin@gmail.com>
2017-10-30 09:54:17 -0600

    Players can now bet and switch turns

--- CardGames/UserControls/BlackJackGame.xaml.cs
+++ CardGames/UserControls/BlackJackGame.xaml.cs
@@ -24,7 +24,7 @@ namespace CardGames.UserControls
     {
         public MainWindow Window { get; set; }
         public BlackJackController Controller { get; set; }
-        public int CurrentPlayer { get; set; }
+        public int CurrentPlayer { get; set; } = 0;
 
         public BlackJackGame(MainWindow w, string[] names)
         {
@@ -36,7 +36,25 @@ namespace CardGames.UserControls
 
         private void Bet_Click(object sender, RoutedEventArgs e)
         {
-            //((BlackJackPlayer)Controller.Players[0]).Bet = 
+            ((BlackJackPlayer)Controller.Players[CurrentPlayer]).Bet = int.Parse(((Button)sender).Content.ToString().Substring(1));
+            //If Last person Make bet invis, make other visable
+            SwitchPlayer();
+        }
+
+        private void SwitchPlayer()
+        {
+            BlackJackPlayerDisplay tmp = (BlackJackPlayerDisplay)cplayerdisplay.Children[1];
+            cplayerdisplay.Children.RemoveAt(1);
+            playerlist.Children.Add(tmp);
+
+            tmp = playerlist.Children[0] as BlackJackPlayerDisplay;
+            playerlist.Children.RemoveAt(0);
+            cplayerdisplay.Children.Add(tmp);
+
+            CurrentPlayer = CurrentPlayer  + 1 == Controller.Players.Count ? 0 : CurrentPlayer + 1;
+            cplayerbet.Content = ((BlackJackPlayer)Controller.Players[CurrentPlayer]).Bet;
+            cplayername.Content = ((BlackJackPlayer)Controller.Players[CurrentPlayer]).Name;
+            bank.Content = ((BlackJackPlayer)Controller.Players[CurrentPlayer]).Bank;
         }
 
         private void Hit_Click(object sender, RoutedEventArgs e)
