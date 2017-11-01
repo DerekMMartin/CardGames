ae47af801127d820847fb8ccae5ce964cd69b778
Matthew Woolhiser <MWoolhiser@student.neumont.edu>
2017-11-01 08:52:16 -0600

    made some changes to GoFish UI

--- CardGames/UserControls/GoFishGame.xaml.cs
+++ CardGames/UserControls/GoFishGame.xaml.cs
@@ -36,14 +36,64 @@ namespace CardGames.UserControls
 
         public void StartGame()
         {
+            SetUpPlayerPositions();
             //Maybe loop for pairs, found pair remove found pair remove
         }
 
+        private void SetUpPlayerPositions()
+        {
+            Binding a = new Binding("Name");
+            a.Source = Game.Players[0];
+            BottomHand.SetBinding(Button.ContentProperty, a);
 
+            switch (Game.Players.Count())
+            {
+                case 2:
+                    TopHand.Visibility = Visibility.Visible;
+                    Binding b = new Binding("Name");
+                    b.Source = Game.Players[1];
+                    TopHand.SetBinding(Button.ContentProperty, b);
+                    break;
+                case 3:
+                    LeftHand.Visibility = Visibility.Visible;
+                    Binding c = new Binding("Name");
+                    c.Source = Game.Players[1];
+                    LeftHand.SetBinding(Button.ContentProperty, c);
+                    RightHand.Visibility = Visibility.Visible;
+                    Binding c2 = new Binding("Name");
+                    c2.Source = Game.Players[2];
+                    RightHand.SetBinding(Button.ContentProperty, c2);
+                    break;
+                case 4:
+                    LeftHand.Visibility = Visibility.Visible;
+                    Binding d = new Binding("Name");
+                    d.Source = Game.Players[1];
+                    LeftHand.SetBinding(Button.ContentProperty, d);
+                    TopHand.Visibility = Visibility.Visible;
+                    Binding d2 = new Binding("Name");
+                    d2.Source = Game.Players[2];
+                    TopHand.SetBinding(Button.ContentProperty, d2);
+                    RightHand.Visibility = Visibility.Visible;
+                    Binding d3 = new Binding("Name");
+                    d3.Source = Game.Players[3];
+                    RightHand.SetBinding(Button.ContentProperty, d3);
+                    break;
+            }
+        }
 
+        private void TopHand_Click(object sender, RoutedEventArgs e)
+        {
 
+        }
 
+        private void LeftHand_Click(object sender, RoutedEventArgs e)
+        {
 
+        }
 
+        private void RightHand_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
